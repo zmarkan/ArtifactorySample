20263d35652b8fd8453240558468bdaaa00b9de2
zan <zan.markan@gmail.com>
2016-01-08 07:26:59 +0000

    Use local library

--- app/src/main/java/com/zmarkan/artifactorysample/MainActivity.java
+++ app/src/main/java/com/zmarkan/artifactorysample/MainActivity.java
@@ -2,6 +2,9 @@ package com.zmarkan.artifactorysample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.zmarkan.echoer.Echo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Echo echoer = new Echo();
+
+        ((TextView)findViewById(R.id.text)).setText(echoer.echo("Hello World!"));
     }
 }
